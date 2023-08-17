2a86c768910d3536b93e0e2acaadabe38bc845ee
NassWebDev <mr.nedjar.nassim@gmail.com>
2023-08-17 14:36:19 +0200

    create navbar component

---
+++ components/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <h1>
+            Instant Gaming
+        </h1>
+        <div class="nav-middle">
+            <nav>
+                <ul>
+                    <li>
+                        Tendances
+                    </li>
+                    <li>
+                        Précommandes
+                    </li>
+                    <li>
+                        Prochaines sorties
+                    </li>
+                </ul>
+            </nav>
+            <input type="text">
+        </div>
+        <div class="icons">
+            <Icon name="ph:basket-light" size="1.8em"/>
+            <Icon name="mdi:person-outline" size="1.8em"/>
+        </div>
+    </header>
+</template>
+
+<style lang="scss">
+    header{
+        display: flex;
+        justify-content: space-around;
+    }
+</style>
