2466eac0a89bc74ec7b57d6e58b984d585a71c01
NassKillers <mr.nedjar.nassim@gmail.com>
2023-09-11 02:18:52 +0200

    add compo (trending-game-image & trending-list)

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue'
+import TrendingGames from '@/components/TrendingGames.vue';
+import TrendingGamesList from '@/components/TrendingGames/List.vue';
 
 useHead({
   title: "Instant Gaming"
@@ -7,7 +9,15 @@ useHead({
 </script>
 
 <template>
-  <div>
+  <div class="main-container">
     <NavBar />
+    <TrendingGames />
+    <TrendingGamesList />
   </div>
 </template>
+
+<style>
+  .main-container{
+    background-color: #272727;
+  }
+</style>
