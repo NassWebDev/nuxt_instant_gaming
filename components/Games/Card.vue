b83a650a398bc37ceff9bf1a731c8e0e21e2fbfd
NassKillers <mr.nedjar.nassim@gmail.com>
2023-09-11 02:20:27 +0200

    create card for game

---
+++ components/Games/Card.vue
@@ -0,0 +1,29 @@
+<template>
+    <img src="@/assets/images/rdr.jpg" :alt=game.name>
+        <div class="game-info">
+            <p>
+            {{ game.name }}
+        </p>
+        <p>
+            {{ game.price }}
+        </p>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  game: Array,
+});
+</script>
+
+<style lang="scss">
+    .game{
+        width: calc(33.33333% - 30px);
+        
+        a{
+            width: 100%;
+            background-image: url("@/assets/images/rdr.jpg");
+            background-size: cover;
+        }
+    }
+</style>
