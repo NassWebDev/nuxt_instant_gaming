3e162f6aec8ad7019095ac080525b472d679dfd6
NassKillers <mr.nedjar.nassim@gmail.com>
2023-09-12 16:36:56 +0200

    add next release component

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
 import NavBar from '@/components/NavBar.vue'
 import TrendingGames from '@/components/TrendingGames.vue';
 import TrendingGamesList from '@/components/TrendingGames/List.vue';
+import PreordersGamesList from '@/components/PreordersGames/List.vue';
 
 useHead({
   title: "Instant Gaming"
@@ -13,11 +14,15 @@ useHead({
     <NavBar />
     <TrendingGames />
     <TrendingGamesList />
+    <PreordersGamesList />
   </div>
 </template>
 
 <style>
   .main-container{
     background-color: #272727;
+    display: flex;
+    flex-direction: column;
+    row-gap: 15px;
   }
 </style>
