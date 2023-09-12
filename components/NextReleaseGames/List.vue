99ddb6a7822d07884b5a8ee1022a708373e4ac5e
NassWebDev <mr.nedjar.nassim@gmail.com>
2023-09-12 17:17:08 +0200

    update name of folder

---
+++ components/NextReleaseGames/List.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="games">
+        <div class="headline">
+            <h3>
+                Prochaines Sorties
+            </h3>
+            <NuxtLink>
+                Tout Voir
+            </NuxtLink>
+        </div>
+        <ul>
+            <li v-for="game in games" :id="game.name">
+                <GamesCard
+                :key="game.name"
+                :game="game"
+                />
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import GamesCard from '@/components/Games/Card.vue';
+
+const games = ref([
+    {
+        name: "Red Dead Redemption 2",
+        price: "59.99$",
+        image: "@/assets/images/rdr.jpg"
+    },
+    {
+        name: "Red Dead Redemption 2",
+        price: "59.99$",
+        image: "@/assets/images/rdr.jpg"
+    },
+    {
+        name: "Red Dead Redemption 2",
+        price: "59.99$",
+        image: "@/assets/images/rdr.jpg"
+    },
+    {
+        name: "Red Dead Redemption 2",
+        price: "59.99$",
+        image: "@/assets/images/rdr.jpg"
+    },
+    {
+        name: "Red Dead Redemption 2",
+        price: "59.99$",
+        image: "@/assets/images/rdr.jpg"
+    },
+    {
+        name: "Red Dead Redemption 2",
+        price: "59.99$",
+        image: "@/assets/images/rdr.jpg"
+    }
+])
+</script>
+
+<style lang="scss">
+@import "@/assets/styles/list.scss";
+</style>
