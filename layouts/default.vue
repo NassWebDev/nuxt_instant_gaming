9ff835b09db48ec4a108c0d9922230b99db26768
NassWebDev <mr.nedjar.nassim@gmail.com>
2023-09-12 17:17:36 +0200

    create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="main-container">
+        <NavBar />
+        <slot />
+    </div>
+</template>
+
+<script setup>
+import NavBar from '@/components/NavBar.vue';
+</script>
+
+<style>
+  .main-container{
+    background-color: #272727;
+    display: flex;
+    flex-direction: column;
+    row-gap: 15px;
+  }
+</style>
