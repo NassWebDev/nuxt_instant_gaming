8547810ba610d4f082c06ca55c68df4c3470b922
NassKillers <mr.nedjar.nassim@gmail.com>
2023-09-14 00:48:01 +0200

    add paginate

--- components/BestRatingGames/List.vue
+++ components/BestRatingGames/List.vue
@@ -9,28 +9,49 @@
             </NuxtLink>
         </div>
         <ul>
-            <li v-for="game in games" :id="game.name">
+            <li v-for="game in allgames?.results" :id="game?.id">
                 <GamesCard
-                :key="game.name"
+                :key="game?.id"
                 :game="game"
                 />
             </li>
         </ul>
+        <vue-awesome-paginate
+            :total-items="12600"
+            :items-per-page="9"
+            :max-pages-shown="3"
+            v-model="currentPage"
+            :on-click="onClickHandler"
+        />
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import GamesCard from '@/components/Games/Card.vue';
 
-const {data: allgames} = await useFetch("https://api.igdb.com/v4/games", {
-    method: "POST",
+const currentPage = ref(1);
+
+const onClickHandler = (page) => {
+    currentPage.value = page;
+    console.log(currentPage.value);
+}
+
+const {data: allgames} = await useFetch(`https://api.rawg.io/api/games?key=${import.meta.env.VITE_API_KEY}&page=${currentPage.value}&page_size=9`, {
+    method: "GET",
     headers:{
-        "Client-ID": "y19n2sixh4hcbrzxn633dx98z671qk",
-        "Authorization": "Bearer us2bxckqupcgkxwm3i1yo7la17x3rd"
-    }
+        "Content-Type": "application/json",
+        "token": `Token ${import.meta.env.VITE_ACCESS_TOKEN}`,
+        "Access-control-allow-origin": "*",
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Methods': '*',
+    },
+    watch:[
+        currentPage
+    ]
 })
 
-console.log(allgames.value);
+console.log(allgames.value?.results);
 
 const games = ref([
     {
@@ -83,4 +104,28 @@ const games = ref([
 
 <style lang="scss">
 @import "@/assets/styles/list.scss";
+.pagination-container {
+    display: flex;
+    column-gap: 10px;
+  }
+  .paginate-buttons {
+    height: 40px;
+    width: 40px;
+    border-radius: 20px;
+    cursor: pointer;
+    background-color: rgb(242, 242, 242);
+    border: 1px solid rgb(217, 217, 217);
+    color: black;
+  }
+  .paginate-buttons:hover {
+    background-color: #d8d8d8;
+  }
+  .active-page {
+    background-color: #3498db;
+    border: 1px solid #3498db;
+    color: white;
+  }
+  .active-page:hover {
+    background-color: #2988c8;
+  }
 </style>
