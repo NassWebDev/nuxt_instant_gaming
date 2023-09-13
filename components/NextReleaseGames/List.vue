5471baa50ed5297860b9d30ff84fe619006e9d48
NassKillers <mr.nedjar.nassim@gmail.com>
2023-09-14 00:48:26 +0200

    add optional chaining

--- components/NextReleaseGames/List.vue
+++ components/NextReleaseGames/List.vue
@@ -9,9 +9,9 @@
             </NuxtLink>
         </div>
         <ul>
-            <li v-for="game in games" :id="game.name">
+            <li v-for="game in allgames.results" :id="game?.id">
                 <GamesCard
-                :key="game.name"
+                :key="game?.id"
                 :game="game"
                 />
             </li>
@@ -21,6 +21,14 @@
 
 <script setup>
 import GamesCard from '@/components/Games/Card.vue';
+
+const {data: allgames} = await useFetch(`https://api.rawg.io/api/games?key=24fe4ee5d92c4643849bd9a18943bafe&ordering=released&page_size=9`, {
+    method: "GET",
+    headers:{
+        "Content-Type": "application/json",
+        "token": "Token f1e9244e5cfaec97e723f5b48967f821a555ec23"
+    }
+})
 
 const games = ref([
     {
