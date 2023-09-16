32165dbcd11d6d5049f6b1e6d97364e31406c9f7
NassKillers <mr.nedjar.nassim@gmail.com>
2023-09-16 23:05:31 +0200

    create next-release

---
+++ pages/next-release.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="games">
+    <h2>
+      Prochaines Sorties
+    </h2>
+    <div class="list">
+      <GamesList :number_of_games="21" :currentPage="currentPage" :ordering="'released'"/>
+      <vue-awesome-paginate
+          :total-items="10000"
+          :items-per-page="21"
+          :max-pages-shown="3"
+          v-model="currentPage"
+          :on-click="onClickHandler"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import GamesList from '@/components/Games/List.vue';
+
+const currentPage = ref(1);
+
+const onClickHandler = (page) => {
+  currentPage.value = page;
+  console.log(currentPage.value);
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/styles/list.scss";
+@import "@/assets/styles/pagination.css";
+</style>
