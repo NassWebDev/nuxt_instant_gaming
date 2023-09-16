1f6dc4a41009c9e40a6cc546f73aac57fd878b04
NassKillers <mr.nedjar.nassim@gmail.com>
2023-09-16 23:04:46 +0200

    create list component

---
+++ components/Games/List.vue
@@ -0,0 +1,44 @@
+<template>
+    <ul class="list-games">
+        <p v-if="pending">
+            <LoadingAnim/>
+        </p>
+        <li v-else v-for="game in toRaw(allgames?.results)" :key="game?.id">
+            <GamesCard
+            :game="game"
+            />
+        </li>
+    </ul>
+</template>
+
+<script setup>
+import GamesCard from '@/components/Games/Card.vue';
+import LoadingAnim from '@/components/LoadingAnim.vue';
+
+const props = defineProps({
+    number_of_games: Number,
+    ordering: String,
+    currentPage: Number
+})
+
+const currentPage = toRef(props, 'currentPage')
+
+const { data: allgames, pending } = await useFetch("https://api.rawg.io/api/games", {
+    method: "GET",
+    headers:{
+        "Content-Type": "application/json",
+        "token": `Token ${import.meta.env.VITE_ACCESS_TOKEN}`,
+    },
+    params:{
+        key: import.meta.env.VITE_API_KEY,
+        page: currentPage,
+        page_size: props?.number_of_games,
+        ordering: props?.ordering
+    },
+    watch:[currentPage]
+})
+</script>
+
+<style lang="scss">
+@import "@/assets/styles/list.scss";
+</style>
