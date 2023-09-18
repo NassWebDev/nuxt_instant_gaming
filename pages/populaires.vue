c55f786882b812128090077abe968aa0e696b3c8
NassKillers <mr.nedjar.nassim@gmail.com>
2023-09-18 19:27:58 +0200

    add list class

--- pages/populaires.vue
+++ pages/populaires.vue
@@ -3,19 +3,25 @@
       <h2>
         Jeux Populaires
       </h2>
-        <GamesList :number_of_games="21" :currentPage="currentPage"/>
-        <vue-awesome-paginate
-            :total-items="10000"
-            :items-per-page="21"
-            :max-pages-shown="3"
-            v-model="currentPage"
-            :on-click="onClickHandler"
-        />
+        <div class="list">
+          <GamesList :number_of_games="21" :currentPage="currentPage"/>
+          <vue-awesome-paginate
+              :total-items="10000"
+              :items-per-page="21"
+              :max-pages-shown="3"
+              v-model="currentPage"
+              :on-click="onClickHandler"
+          />
+        </div>
     </div>
 </template>
 
 <script setup>
 import GamesList from '@/components/Games/List.vue';
+
+definePageMeta({
+  layout: "custom"
+})
 
 const currentPage = ref(1);
 
