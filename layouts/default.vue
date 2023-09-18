e2051f1026662b662011c158da1baaf847b02277
NassKillers <mr.nedjar.nassim@gmail.com>
2023-09-18 19:26:22 +0200

    add trending game image

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,22 @@
 <template>
-    <div class="main-container">
-        <NavBar />
-        <slot />
-    </div>
+  <div class="main-container">
+      <NavBar />
+      <TrendingGames />
+      <slot />
+  </div>
 </template>
 
 <script setup>
 import NavBar from '@/components/NavBar.vue';
+import TrendingGames from '@/components/TrendingGames.vue';
 </script>
 
 <style>
   .main-container{
-    background-color: #272727;
-    display: flex;
-    flex-direction: column;
-    row-gap: 15px;
-    min-height: 100vh;
+      background-color: #272727;
+      display: flex;
+      flex-direction: column;
+      row-gap: 15px;
+      min-height: 100vh;
   }
 </style>
