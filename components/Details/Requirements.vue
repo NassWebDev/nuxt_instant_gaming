bedae76da1d0259330a3390902e55b5b9f8601ab
NassWebDev <mr.nedjar.nassim@gmail.com>
2023-09-22 01:37:24 +0200

    create Requirements component

---
+++ components/Details/Requirements.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="requirements-container">
+        <h2>
+            Requirements
+        </h2>
+        <div class="requirements">
+            <p v-html="formattedMinimum"></p>
+            <p v-html="formattedRecommended"></p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    game: Object
+})
+
+const minimum = props?.game?.requirements?.minimum;
+const formattedMinimum = minimum?.replace(/\n/g, "<br>");
+
+const recommended = props?.game?.requirements?.recommended;
+const formattedRecommended = recommended?.replace(/\n/g, "<br>");
+</script>
+
+<style lang="scss">
+    .requirements-container{
+      display: flex;
+      flex-direction: column;
+      row-gap: 20px;
+
+      .requirements{
+        display: flex;
+        column-gap: 100px;
+
+        p{
+          width: calc(50% - 30px);
+          line-height: 30px;
+          font-size: 20px;
+        }
+      }
+    }
+</style>
