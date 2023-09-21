d9ceec81a40f0ffb5336202f2dc3c886b2b22bc8
NassKillers <mr.nedjar.nassim@gmail.com>
2023-09-21 19:47:01 +0200

    add module carousel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['nuxt-icon'],
+  modules: ['nuxt-icon', 'vue3-carousel-nuxt'],
   css: [`@/assets/styles/reset.css`],
 })
