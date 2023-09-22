2d8160d7a89572835eb16dfdee381f9129e9a586
NassKillers <mr.nedjar.nassim@gmail.com>
2023-09-22 20:59:25 +0200

    show dynamic image

--- components/TrendingGames.vue
+++ components/TrendingGames.vue
@@ -1,12 +1,53 @@
 <template>
-    <div class="imageTrending">
+    <div class="imageTrending" :style="{ backgroundImage: 'url(' + image + ')' }">
     </div>
 </template>
+
+<script setup>
+const route = useRoute();
+const router = useRouter();
+
+const image = ref(null);
+
+const { data: trendingGame } = await useFetch(`https://api.rawg.io/api/games`, {
+    method: "GET",
+    headers:{
+        "Content-Type": "application/json",
+        "token": `Token ${import.meta.env.VITE_ACCESS_TOKEN}`,
+    },
+    params:{
+        key: import.meta.env.VITE_API_KEY
+    }
+})
+
+const { data: game } = await useFetch(`https://api.rawg.io/api/games/${route.params.slug}`, {
+    method: "GET",
+    headers:{
+        "Content-Type": "application/json",
+        "token": `Token ${import.meta.env.VITE_ACCESS_TOKEN}`,
+    },
+    params:{
+        key: import.meta.env.VITE_API_KEY
+    }
+})
+
+if(router.currentRoute.value.path === `/game/${route.params.slug}`){
+    image.value = game.value?.background_image_additional;
+}
+else{
+    if(trendingGame.value?.results[0]?.background_image_additional){
+        image.value = trendingGame.value?.results[0]?.background_image_additional;
+    }
+    else{
+        image.value = trendingGame.value?.results[0]?.background_image;
+    }
+}
+</script>
 
 <style lang="scss">
     .imageTrending{
         height: 80vh;
-        background-image: url("@/assets/images/rdr.jpg");
+        // background-image: url("@/assets/images/rdr.jpg");
         background-size: cover;
         clip-path: polygon(0 0, 100% 0, 100% 100%, 0 85%);
 
