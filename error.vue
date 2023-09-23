e4a588f430d1e95d9987713d3f67198fcf1400c6
NassWebDev <mr.nedjar.nassim@gmail.com>
2023-09-23 13:38:20 +0200

    better style

--- error.vue
+++ error.vue
@@ -1,3 +1,17 @@
+<template>
+  <div class="error-container">
+    <h1 class="error-status">
+      {{ error.statusCode }}
+    </h1>
+    <p class="error-message">
+      {{ error.message }}
+    </p>
+    <button @click="handleError">
+      Go Back
+    </button>
+  </div>
+</template>
+
 <script setup>
 const error = useError();
 
@@ -8,15 +22,40 @@ const handleError = () => {
 };
 </script>
 
-<template>
-  <div class="flex h-screen justify-center items-center flex-col">
-    <h1 class="text-9xl">{{ error.statusCode }}</h1>
-    <p class="mt-7 text-4xl">{{ error.message }}</p>
-    <button
-      @click="handleError"
-      class="rounded mt-7 text-2xl bg-blue-400 px-7 py-4 text-white"
-    >
-      Go Back
-    </button>
-  </div>
-</template>
+<style lang="scss">
+.error-container{
+  height: 100vh;
+  background-color: #272727;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 20px;
+
+  .error-status{
+    font-size: 150px;
+    color: #fff;
+  }
+
+  .error-message{
+    font-size: 30px;
+    color: #fff;
+  }
+
+  button{
+    width: 150px;
+    height: 40px;
+    border: none;
+    border-radius: 8px;
+    font-size: 25px;
+    background-color: #ff6000;
+    color: #fff;
+    cursor: pointer;
+    transition: all 0.150s ease;
+
+    &:hover{
+      transform: scale(1.1);
+    }
+  }
+}
+</style>
