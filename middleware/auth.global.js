a5e1390f60626596d0042375c26ff84a7f488716
NassKillers <mr.nedjar.nassim@gmail.com>
2023-09-29 00:50:33 +0200

    add option to middleware

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  if(!to.path.includes("login") && !to.path.includes("register")) {
+  if(!to.path.includes("login") && !to.path.includes("register") && !to.path.includes("")) {
     const user = useSupabaseUser();
     if(!user.value) {
       return navigateTo("/login");
