786c01d0f5fa5a863e466caab2f80ffdcdbe1fce
NassKillers <mr.nedjar.nassim@gmail.com>
2023-09-29 00:35:42 +0200

    update auth middleware

---
+++ middleware/auth.global.js
@@ -0,0 +1,8 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  if(!to.path.includes("login") && !to.path.includes("register")) {
+    const user = useSupabaseUser();
+    if(!user.value) {
+      return navigateTo("/login");
+    }
+  }
+})
