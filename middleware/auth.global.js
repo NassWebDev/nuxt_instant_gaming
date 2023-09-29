8734080d08ea78ccf633b2871032b1a1b7985720
NassKillers <mr.nedjar.nassim@gmail.com>
2023-09-29 07:09:57 +0200

    better condition

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  if(!to.path.includes("login") && !to.path.includes("register") && !to.path.includes("")) {
+  if(!to.path.includes("login") && !to.path.includes("register")) {
     const user = useSupabaseUser();
     if(!user.value) {
       return navigateTo("/login");
