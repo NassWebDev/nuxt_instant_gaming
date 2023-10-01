bfdc0dd6c44c83bc7123af8b212b6fcb7d2c7310
NassWebDev <mr.nedjar.nassim@gmail.com>
2023-10-01 02:35:54 +0200

    change data to seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -5,24 +5,13 @@ const prisma = new PrismaClient();
 async function seed() {
     const user = await prisma.user.create({
         data:{
-            firstName: 'John',
-            lastName: 'Doe',
+            userName: 'Doe',
             email: '7VcD4@example.com',
             dateOfBirth: '2000-01-01',
-            favorites:{
-                create:[
-                    {
-                        gameSlug: 'red-dead-redemption-2',
-                        gameTitle: 'Red Dead Redemption 2',
-                        gameImage: 'https://media.rawg.io/media/games/511/5118aff5091cb3efec399c808f8c598f.jpg'
-                    },
-                    {
-                        gameSlug: 'elden-ring',
-                        gameTitle: 'Elden Ring',
-                        gameImage: 'https://media.rawg.io/media/games/b29/b294fdd866dcdb643e7bab370a552855.jpg'
-                    },
-                ]
-            },
+            favorites:[
+                'red-dead-redemption-2',
+                'elden-ring'
+            ],
             comments:{
                 create:[                 
                     {
@@ -37,7 +26,6 @@ async function seed() {
             }
         }
     })
-    console.log(user)
 }
 
 seed()
