5b5d70be6fc50d68143d932f5edb7d26e97a449e
NassWebDev <mr.nedjar.nassim@gmail.com>
2023-10-02 16:33:47 +0200

    delete to test

--- middleware/auth.js
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  if(!to.path.includes("login") && !to.path.includes("register")) {
-    const user = useSupabaseUser();
-    if(!user.value) {
-      return navigateTo("/login");
-    }
-  }
-})
