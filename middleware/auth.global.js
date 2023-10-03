db57cc8653825eb10502ee864556dfcd0c7cfa19
NassKillers <mr.nedjar.nassim@gmail.com>
2023-10-03 17:37:32 +0200

    add update-password route

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    if(!to.path.includes("login") && !to.path.includes("register")) {
+    if(!to.path.includes("login") && !to.path.includes("register") && !to.path.includes("forgot-password") && !to.path.includes("update-password")) {
       const user = useSupabaseUser();
       if(!user.value) {
         return navigateTo("/login");
