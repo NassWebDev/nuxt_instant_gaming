4d579ab5c1ccfafcccf810ee456704fbcb18d620
NassKillers <mr.nedjar.nassim@gmail.com>
2023-10-03 05:31:59 +0200

    create log layout

---
+++ layouts/log.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="login-container">
+        <slot />
+        <img src="../assets/images/instant-gaming-connexion.jpg" alt="">
+    </div>
+</template>
+  
+<style lang="scss">
+.login-container {
+    width: 100%;
+    height: 100vh;
+    background-color: #323232;
+    display: flex;
+
+    img{
+        width: 50%;
+        object-fit: cover;
+        transition: all 0.3s ease;
+
+        @media screen and (max-width: 1000px) {
+            display: none;
+        }
+    }
+}
+
+  </style>
