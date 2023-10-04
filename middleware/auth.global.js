5b4c1def82313b873e9bdd2401ad935b25ee1ec4
Nass <90240661+NassWebDev@users.noreply.github.com>
2023-10-05 01:47:57 +0200

    Add condition

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    if(to.path.includes("game")) {
+    if(to.path.includes("game") && to.path.includes("my-account")) {
         const user = useSupabaseUser();
         if(!user.value) {
             return navigateTo("/profile/login");
