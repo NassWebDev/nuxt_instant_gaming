9b6998e06a4c73139ab27237ad326c22d2942824
NassKillers <mr.nedjar.nassim@gmail.com>
2023-10-04 02:12:08 +0200

    add script to add user logged from oauth

--- app.vue
+++ app.vue
@@ -5,3 +5,32 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+const user = useSupabaseUser();
+
+if(user) {
+  const userEmail = user.value?.email;
+  const userName = user.value?.user_metadata?.full_name;
+
+  const {error, data} = await useFetch(`/api/user/${userEmail}`);
+
+  if(error.value) {
+    console.log(error.value)
+  }
+
+  if(!data.value) {
+    const {errorFetch} = await useFetch('/api/user/user', {
+      method: 'POST',
+      body: {
+        email: userEmail,
+        userName: userName,
+        dateOfBirth: new Date().toISOString().split('T')[0]
+      }
+    })
+    if (errorFetch) {
+      console.log(errorFetch);
+    }
+  }
+}
+</script>
