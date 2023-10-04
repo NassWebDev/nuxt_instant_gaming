0e602763f6b88796d3012d6db41626340f816573
Nass <90240661+NassWebDev@users.noreply.github.com>
2023-10-05 01:50:11 +0200

    Update auth.global.js

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    if(to.path.includes("game") && to.path.includes("my-account")) {
+    if(to.path.includes("game") || to.path.includes("my-account")) {
         const user = useSupabaseUser();
         if(!user.value) {
             return navigateTo("/profile/login");
